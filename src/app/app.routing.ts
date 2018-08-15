5901b82c7be57ada638dc2f9ebc0f73e7dec58dc
JoanAndoni <mcr_joan@hotmail.com>
2018-08-15 15:19:37 -0500

    Add all that you can do with routing and a nav-bar

---
+++ src/app/app.routing.ts
@@ -0,0 +1,22 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+//Importar componentes
+import { FrutaComponent } from './fruta/fruta.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
+import { HomeComponent } from './home/home.component';
+import { ContactoComponent } from './contacto/contacto.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'empleado', component: EmpleadoComponent},
+  {path: 'fruta', component: FrutaComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'contacto', component: ContactoComponent},
+  {path: 'contacto/:variable', component: ContactoComponent},
+  {path: '**', component: HomeComponent}
+];
+
+export const appRoutingProviders: any[] = [];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);

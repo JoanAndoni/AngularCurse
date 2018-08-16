1c87e982301197e247c88348e9fd809343dd510b
JoanAndoni <mcr_joan@hotmail.com>
2018-08-16 10:37:36 -0500

    Create a service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { RopaService } from '../services/ropa.service';
 
 @Component({
   selector: 'home',
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  providers: [RopaService]
 })
 
 export class HomeComponent{
   public titulo = "Página principal";
+  public valorServicio:string;
+
+  constructor(
+    private _ropaService: RopaService
+  ){}
+
+  ngOnInit(){
+    this.valorServicio = this._ropaService.prueba('Camizon');
+  }
 }
